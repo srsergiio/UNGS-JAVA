7d908cecac274eba6f343d2d91e3be754fc510ab
srsergiio <sesquivelsaibene@gmail.com>
2021-12-27 01:10:41 -0300

    ejercicio 1 cap 7

--- Programacion 1/src/C07_Cadenas_y_Cosas/E71_Prueba_Contatenacion.java
+++ Programacion 1/src/C07_Cadenas_y_Cosas/E71_Prueba_Contatenacion.java
@@ -1,10 +1,62 @@
 package C07_Cadenas_y_Cosas;
-
-public class E71 {
+import Libro.*;
+public class E71_Prueba_Contatenacion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		sumaStringChart("SERGIO.",'E');/*SUMA DE STRING + CHAR FORMAN UN STRING*/
+		suma_Boolean_String(true,"sergio") ;
+		suma_Char_Char('E','E');
+	}
+	
+	
+	
+	public static void sumaStringChart(String palabra,char letra) {
+		  String TipodeArchivopordefecto = palabra+letra;
+		print.print("String + Char = String : "+TipodeArchivopordefecto);
+	}
+	
+	
+	public static void suma_Boolean_boolean(boolean a,boolean b) {
+		  print.print(a+b);// no se puede sumar boolean
+	}
+	
+	public static void suma_Boolean_char(boolean a,char b) {
+			Object TipodeArchivopordefecto = a+b;
+		  print.print(a+b);// no se puede sumar char y boolean
+	}
+	
+	public static void suma_Boolean_char(boolean a,int b) {
+		  Object TipodeArchivopordefecto = a+b;
+	  print.print(a+b);// no se puede sumar char y INT
+	}
+	
+	public static void suma_Boolean_String(boolean a,String b) {
+		  String TipodeArchivopordefecto = a+b;
+	  print.print("Boolean + String = String : "+a+b);
+	}
+	
+	public static void suma_Char_Char(char a,char b) {
+		int     TipodeArchivopordefecto = a+b;
+		print.print("char + char = int : "+TipodeArchivopordefecto);
+		
+	}
+	
+	public static void suma_Char_int(char a,int b) {
+		      int TipodeArchivopordefecto = a+b;
+	  print.print("char + int = int : "+a+b);
+	}
+	
+	public static void suma_Char_Str(char a,String b) {
+	        String TipodeArchivopordefecto = a+b;
+	        print.print("char + String = String : "+ a+b); 
+	}
+	
+	public static void suma_INT_Str(int a,String b) {
+          String TipodeArchivopordefecto = a+b;
+        print.print("INT + STRING = STRING : "+a+b);// 
 	}
 
+	
+	
 }
