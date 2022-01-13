414e389a8bc39510b63deac127aa184501633df0
srsergiio <sesquivelsaibene@gmail.com>
2022-01-12 23:38:50 -0300

    ordenanar un arr desordenado

---
+++ 00_Programacion 1/src/C10_Arreglos/E110_OrdenARR.java
@@ -0,0 +1,84 @@
+package C10_Arreglos;
+
+import LES.print;
+
+public class E110_OrdenARR {
+//	Una manera no muy eficiente de ordenar los elementos de un arreglo es 
+//	encontrar el mayor e intercambiarlo con el primero, después encontrar el segundo
+//	mayor, e intercambiarlo con el segundo, y así siguiendo.
+//	
+public static void main (String []sljdhgfoliksdfjhglksdfg) {
+	int [] arr = {1,3,5,6,4,2};
+	
+	
+	
+	arr = ordenarArreglo( arr);
+
+	printarr(arr);
+	
+	
+}
+
+		public static int indiceDeMaximoEnRango (int [] arr , int indiceBajo ){
+		 
+//				a. Escribí un método llamado indiceDeMaximoEnRango que tome un arreglo
+//				de enteros, encuentre el mayor elemento en el rango dado, y devuelva su
+//				índice. Podés modificar tu versión recursiva de maximoEnRango o empezar
+//				una versión iterativa desde cero.
+			//	
+		int MaxElemento = arr[indiceBajo];
+		int index = 0;
+		if (indiceBajo >=0 && indiceBajo<arr.length) {
+			for (int i = indiceBajo; i < arr.length; i++) {
+				if(arr[i]>=MaxElemento) {
+					MaxElemento=arr[i];
+					 index = i;
+					
+				}
+				
+			}
+		}
+		return index;
+		
+	}
+		
+		public static int [] intercambiarElemento (int [] arr , int a,int b) {
+//			b. Escribí un método llamado intercambiarElemento que tome un arreglo de
+//			enteros y dos índices, y que intercambie los elementos de los índices dados.
+		//	
+			int guardar =arr[a];
+			arr[a] =arr[b];
+			arr[b] =guardar;
+			return arr;
+		}
+		
+		
+		public static int [] ordenarArreglo(int []arr){
+			int[] arr1 = arr;
+//			c. Escribí un método llamado ordenarArreglo que tome un arreglo de enteros
+//			y que use indiceDeMaximoEnRango e intercambiarElemento para ordenar
+//			el arreglo de mayor a menor.
+			for (int i = 0; i < arr1.length; i++) {
+				 arr1 = intercambiarElemento (arr,i,indiceDeMaximoEnRango (arr ,  i));	
+			}			
+			return arr;
+		}
+
+		public static void printarr(int []arr) {
+			System.out.print( " [  ");
+			for (int i : arr) {
+				System.out.print(i+" , ");
+			}
+			System.out.print( " ]  ");
+		}
+		
+		
+		
+		
+		
+		
+	}
+		
+		
+		
+
